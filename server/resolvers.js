bce90428395a6206b53a1d95e23b63bfc9343c02
Phat Nguyen <preddevil@gmail.com>
2024-09-24 21:01:42 +0700

    Add Job and Company schema

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,16 @@
+import { getJobs } from "./db/jobs.js";
+import { getCompany } from "./db/companies.js";
+
 export const resolvers = {
   Query: {
-    greeting: () => "Hello World",
+    jobs: getJobs,
+  },
+  Job: {
+    date: (job) => toISODate(job.createdAt),
+    company: (job) => getCompany(job.companyId),
   },
 };
+
+function toISODate(value) {
+  return value.slice(0, "yyyy-mm-dd".length);
+}
